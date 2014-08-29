76630c5c0176e594ff649f2670ece1ddd7761a7a
Konstantin Popov <konstantin.popov.89@yandex.ru>
2014-08-29 16:24:34 +0400

    Добавил название альбома и дату загрузки фотографии во всплывающее окно.
    Они обновляются при навигации по галерее.

--- irada.su/themes/photos/irada/irada.photos.js
+++ irada.su/themes/photos/irada/irada.photos.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 	var bottomBar = $('<div/>').addClass('mfp-bottom-bar')
 		.append($('<div/>').addClass('mfp-counter'))
 		.append($('<div/>').addClass('date').html('Добавлена <span class="val"></span>'))
-		.append($('<div/>').addClass('albumname').text('Альбом'));
+		.append($('<div/>').addClass('albumname').html('Альбом <span class="val"></span>'));
 	// Иконки навигации по галерее
 	var nav = $('<div/>').addClass('nav-left').add($('<div/>').addClass('nav-right'));
 	// Соберём воедино
@@ -32,7 +32,20 @@ $(document).ready(function () {
 			enabled: true,
 			tCounter: '<span class="">Фотография<br />%curr% / %total%</span>',
 			arrowMarkup: ''
-		},	
+		},
+		callbacks: {
+			change: function () {
+				var $el = this.currItem.el,
+					uploaddate = new Date($el.data('uploaddate').split(' ', 1)[0]),
+					albumname = $el.data('albumname') ? "&laquo;" + $el.data('albumname') + "&raquo;" : "";
+				this.content
+					.find('.mfp-bottom-bar .date > .val')
+						.text(uploaddate.toLocaleDateString())
+						.end()
+					.find('.mfp-bottom-bar .albumname > .val')
+						.html(albumname);
+			}
+		}
 	});
 
 	// Навигация
