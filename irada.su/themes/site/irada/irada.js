ed1071da419e2bd40bc825499282a62f0bf07e09
Konstantin Popov <konstantin.popov.89@yandex.ru>
2014-09-11 13:20:21 +0400

    Изменена страница личных данных.
    Адрес убран (он не используется), добавлены поля, в том числе, поле
    "День рождения" с хитрым календариком. Подробности - в комментах
    к js-коду в irada.js в теме сайта. Кроме того, убрал блокировку полей
    от изменения (и, соответственно кнопку "Изменить").
    В сборку jquery-ui добавлен виджет календаря.

--- irada.su/themes/site/irada/irada.js
+++ irada.su/themes/site/irada/irada.js
@@ -1,14 +1,5 @@
 $(document).ready(function () {
 
-	// Выключим по-умолчанию некоторые формы (до нажатия определённой кнопки)	
-	$('form.disabled-by-default input, form.disabled-by-default select').prop('disabled', true);
-	// Включатель форм
-	$('.makeeditable').on('click', function (event) {
-		event.preventDefault();
-		$('#' + $(this).attr('rel') + ' input').prop('disabled', false);
-		$('#' + $(this).attr('rel') + ' select').prop('disabled', false);
-	});
-
 	// Ссылки "Добавить в избранное"
 	$('a.favourite').on('click', function () {
 		var title = window.document.title, // можно на что-то заменить
@@ -29,5 +20,70 @@ $(document).ready(function () {
 		}
 		return false;
 	});
+
+	// Локализация календаря
+	$.datepicker.regional['ru'] = {
+		closeText: 'Закрыть',
+		prevText: '&#x3c;Пред',
+		nextText: 'След&#x3e;',
+		currentText: 'Сегодня',
+		monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+			'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+		monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+			'Июл','Авг','Сен','Окт','Ноя','Дек'],
+		dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+		dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+		dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+		dateFormat: 'dd.mm.yy',
+		firstDay: 1,
+		isRTL: false
+	};
+	$.datepicker.setDefaults($.datepicker.regional['ru']); 
+
+	// Календарь для выбора даты рождения в профиле пользователя.
+	// Чтобы использовать стандартные параметры пользователя WebAsyst,
+	// будем прятать его поля, создавать своё поле с $.datepicker и
+	// связывать его со спрятанными.
+	var $bdField = $('form#userprofile .wa-field.wa-field-birthday');
+	if ($bdField.length) {
+		var bdDay = $bdField.find('[name="profile[birthday][value][day]"]').val(),
+			bdMonth = $bdField.find('[name="profile[birthday][value][month]"]').val(),
+			bdYear = $bdField.find('[name="profile[birthday][value][year]"]').val();
+		
+		// Создаём наше поле
+		var $fakeBirthdayField = 
+			$('<input type="text" name="bd" />')
+			.datepicker()
+			.on('change', function () {
+				var d = $(this).datepicker('getDate');
+				$bdField
+					.find('[name="profile[birthday][value][day]"]')
+						.val(d.getDate())
+						.end()
+					.find('[name="profile[birthday][value][month]"]')
+						.val(d.getMonth() + 1)
+						.end()
+					.find('[name="profile[birthday][value][year]"]')
+						.val(d.getFullYear())
+						.end();
+
+			});
+		
+		// Заполняем его данными из стандартных полей
+		if (bdDay && bdMonth && bdYear) {
+			$fakeBirthdayField.datepicker('setDate', new Date(bdYear, bdMonth - 1, bdDay))
+		}
+		
+		// Спрячем стандартные поля и вставим наше
+		$bdField.find('select, input').hide();
+		$bdField.find('.wa-value').append($fakeBirthdayField);
+
+		// Иконка календаря
+		var $calendarIcon = $('<div/>').addClass('calendarIcon')
+			.on('click', function () {
+				$fakeBirthdayField.datepicker('show');
+			});
+		$fakeBirthdayField.after($calendarIcon);
+	}
 
 });
